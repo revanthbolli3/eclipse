fd48ce13f88cc370fd30a1b11b3e954312d2849b
revanthbolli3 <revanth.bolli@zemosolabs.com>
2022-12-10 18:12:16 +0530

    First Commit on Test1

---
+++ src/Test1.java
@@ -0,0 +1,9 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("This is Test1 class");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author revan
+ *
+ */
+module GitwithEclipseIDE {
+}
